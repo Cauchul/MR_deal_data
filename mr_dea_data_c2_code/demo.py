ec799070e590e950af3ce8ea92fb7f6c5f67452c
liyankang <yankang.li@dinglicom.com>
2024-01-08 09:15:30 +0800

    deal data tool

--- mr_dea_data_c2_code/demo.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-data_path = r'D:\working\1206_国际财经中心测试V1\场景1\8539\LTE'
-
-segments_slash = data_path.split("\\")[-2]
-
-print('segments_slash: ', segments_slash)
--- new_mr_add_umer/demo.py
+++
@@ -1,5 +0,0 @@
-from DataPreprocessing import DataPreprocessing
-
-a = [1699446797323, 1699446797323]
-res = DataPreprocessing.convert_timestamp_to_datetime(a)
-print(res)
